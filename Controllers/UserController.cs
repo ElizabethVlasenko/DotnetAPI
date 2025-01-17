a3d1a9fd1c20e5d57d597a017b1eed471ec56d1f
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2025-01-17 16:49:31 +0000

    Add a custom controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        [HttpGet("GetUsers/{testValue}")]
+        //public IActionResult Test()
+        public string[] GetUsers(string testValue)
+        {
+            string[] responceArray = new string[] { "test1", "test2", testValue };
+            return responceArray;
+        }
+    }
+}
+
