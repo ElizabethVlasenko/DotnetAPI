196c4818b4a15d7d175e3b10c2fc79597c5fd8af
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2025-01-20 11:12:25 +0000

    Connect to the database

---
+++ Data/DataContextDapper.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DotnetAPI.Data
+{
+    public class DataContextDapper
+    {
+        private readonly IConfiguration _config;
+
+        public DataContextDapper(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IEnumerable<T> LoadData<T>(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbconnection.Query<T>(sql);
+
+        }
+
+        public T LoadDataSingle<T>(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbconnection.QuerySingle<T>(sql);
+
+        }
+
+        public Boolean ExecuteSql(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return (dbconnection.Execute(sql) > 0);
+        }
+
+        public int ExecuteSqlWithRowCount(string sql)
+        {
+            IDbConnection dbconnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbconnection.Execute(sql);
+        }
+    }
+}
