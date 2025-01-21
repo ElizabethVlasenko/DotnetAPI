661c04c5a1716c422b2a25c44d0d66aadc792e6f
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2025-01-21 12:39:46 +0000

    Add data context for entity framework

---
+++ Data/DataContextEF.cs
@@ -0,0 +1,47 @@
+﻿using DotnetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetAPI.Data
+{
+    public class DataContextEF : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public DataContextEF(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<UserSalary> UserSalary { get; set; }
+        public virtual DbSet<UserJobInfo> UserJobInfo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                        .UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                            optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("TutorialAppSchema"); //override a default schema name
+
+            modelBuilder.Entity<User>()
+                .ToTable("Users", "TutorialAppSchema") // set up the right table as the class name is User that should be connected to Users table
+                .HasKey(u => u.UserId); //set up a primary key
+
+            modelBuilder.Entity<UserSalary>()
+                .HasKey(u => u.UserId);
+
+            modelBuilder.Entity<UserJobInfo>()
+                .HasKey(u => u.UserId);
+        }
+
+
+    }
+}
