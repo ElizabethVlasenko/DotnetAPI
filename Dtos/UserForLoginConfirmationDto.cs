b68c79d5f8064a67ad6449fe3913fc813dff9ead
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2025-01-22 15:57:18 +0000

    Add Dtos for user Auth

---
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DotnetAPI.Dtos
+{
+    partial class UserForLoginConfirmationDto
+    {
+        byte[] PasswordHash { get; set; } = new byte[0];
+        byte[] PasswordSalt { get; set; } = new byte[0];
+    }
+}
